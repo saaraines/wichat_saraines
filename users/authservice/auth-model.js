f32f3f4d6a436d281c206b2d04977b8e3fa79c29
uo277494 <uo277494@uniovi.es>
2025-10-15 22:51:18 +0200

    Editando el modelo de los usuarios para crear el admin

--- users/authservice/auth-model.js
+++ users/authservice/auth-model.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
     username: String,
     password: String,
     createdAt: Date,
+    role: String,
+    isBlocked: Boolean,
 });
 
 const User = mongoose.model('User', userSchema);
--- users/userservice/user-model.js
+++ users/userservice/user-model.js
@@ -9,6 +9,18 @@ const userSchema = new mongoose.Schema({
       type: String,
       required: true,
     },
+    // nuevo: tipo de usuario
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+
+    // nuevo: bloqueado o no
+    isBlocked: {
+        type: Boolean,
+        default: false
+    },
     createdAt: {
       type: Date,
       default: Date.now, 
